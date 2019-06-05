57096a3780cff4a47b78c814b27274992ef67348
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-05 10:47:14 +0100

    stutured the folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router,Route , Switch} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () =>(
+    <Router>
+        <Switch>
+            <Route  exact path="/" component ={App}/>
+            <Route  path="/login" component ={Login}/>
+            <Route  path="/register" component ={Register}/>
+        </Switch>
+    </Router>
+)
+
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
