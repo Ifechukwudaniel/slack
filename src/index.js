395dbd55aaff23cbb2aabb09212348e8c62af478
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-06 00:56:27 +0100

    added functonality  redirect  user to home Page is  Signedin

--- src/index.js
+++ src/index.js
@@ -5,20 +5,37 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router,Route , Switch} from 'react-router-dom'
+import { BrowserRouter as Router,Route , Switch, withRouter} from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
+import firebase  from './firebase'
+import { Provider } from 'react-redux'
+import {createStore} from 'redux'
+import {re} from 'redux-devtools-extension'
 
-const Root = () =>(
+class  Root extends React.Component {
+    componentDidMount (){
+      firebase.auth().onAuthStateChanged(user=>{
+         if( user){
+             this.props.history.push('/')
+         }
+      })
+    }
+    render(){
+        return(
+                <Switch>
+                    <Route  exact path="/" component ={App}/>
+                    <Route  path="/login" component ={Login}/>
+                    <Route  path="/register" component ={Register}/>
+                </Switch>
+        )
+    }
+}
+
+ const RootwithAuth = withRouter(Root)
+
+
+ReactDOM.render(
     <Router>
-        <Switch>
-            <Route  exact path="/" component ={App}/>
-            <Route  path="/login" component ={Login}/>
-            <Route  path="/register" component ={Register}/>
-        </Switch>
-    </Router>
-)
-
-
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+        <RootwithAuth/>
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
