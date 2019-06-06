594a1f1a5c083db80643df07081d1f1b154c9814
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-06 22:05:56 +0100

    made a correction

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Menu } from 'semantic-ui-react'
-import UserPanel from "./UersPanel"
+import UserPanel from "./UserPanel"
 
  class SidePanel extends Component {
   render() {
