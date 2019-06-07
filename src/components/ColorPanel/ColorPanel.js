be4bcb8f908bccbc9bbba0af2bee26d852dc04ac
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-07 16:04:41 +0100

    added messsage Component and scaffloding

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
 
-class ColorPanel extends Component {
-    render() {
-        return (
-            <div>
-                ColorPanel
-            </div>
-        )
-    }
+class ColorPanel extends React.Component {
+  render() {
+    return (
+      <Sidebar
+        as={Menu}
+        icon="labeled"
+        inverted
+        vertical
+        visible
+        width="very thin"
+      >
+        <Divider />
+        <Button icon="add" size="small" color="blue" />
+      </Sidebar>
+    );
+  }
 }
-export default ColorPanel
+
+export default ColorPanel;
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
 
- class Messages extends Component {
-    render() {
-        return (
-            <div>
-                 Messages
-            </div>
-        )
-    }
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+
+class Messages extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </React.Fragment>
+    );
+  }
 }
 
-export default Messages
+export default Messages;
