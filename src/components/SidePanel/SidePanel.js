9259177e7320dcfead060d3659a82cb75463b1a9
Ifecukwu daniel <mailidanielifecukwu@gmail.com>
2019-06-08 09:22:59 +0100

    edit

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -9,7 +9,7 @@ import Channels from './Channels'
     return (
         <Menu size="large" fixed="left" vertical style = {{background: '#4c3c4c' ,fontSize:'1.2rem'}}>
           <UserPanel currentUser={currentUser}/>
-          <Channels currentUser={currentUser}/>
+          <Channels currentUser={currentUser} />
         </Menu>
       
     );
